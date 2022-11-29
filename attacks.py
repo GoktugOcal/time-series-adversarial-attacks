75dd6a278ae0baf500dc8d8ec1002a1c44d1adc9
goktugocal <goktugocal41@gmail.com>
2022-11-30 00:08:42 +0300

    start

--- attacks.py
+++ attacks.py
@@ -1,2 +1,43 @@
 # FGSM
 # PGD
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense,Dropout,LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.losses import mean_squared_error
+from tensorflow.keras.backend import sign
+
+
+def fgsm_attack(X_test, y_test, model, epsilon):
+    inp = tf.convert_to_tensor(X_test, dtype = tf.float32)
+    imgv = tf.Variable(inp)
+    with tf.GradientTape() as tape:
+        tape.watch(imgv)
+        predictions = model(imgv)
+        loss = tf.keras.losses.MeanSquaredError()(y_test, predictions)
+        grads = tape.gradient(loss,imgv)
+    signed_grads = tf.sign(grads)
+    inp = inp + (epsilon*signed_grads)
+
+    return inp
+
+def pgd_attack(X_test, y_test, model, iterations, alpha, epsilon):
+    
+    gen_img = tf.convert_to_tensor(X_test, dtype=tf.float32)
+    gen_img = tf.clip_by_value(gen_img, X_test-epsilon, X_test+epsilon)
+    x_temp = X_test
+
+    for iters in range(iterations):
+        imgv = tf.Variable(gen_img)
+        with tf.GradientTape() as tape:
+            tape.watch(imgv)
+            predictions = model(imgv)
+            loss = tf.keras.losses.MeanSquaredError()(y_test, predictions)
+            grads = tape.gradient(loss,imgv)
+
+        signed_grads = tf.sign(grads)
+        gen_img = gen_img + (alpha*signed_grads)
+        gen_img = tf.clip_by_value(gen_img, X_test-epsilon, X_test+epsilon)
+
+        
+    return gen_img
--- models.py
+++ models.py
@@ -4,3 +4,54 @@
 # BDSLTM encoder - LSTM decoder
 # CNN
 # CNN + LSTM
+
+from keras.layers import Bidirectional
+
+def single_layer_lstm(input_shape: tuple):
+
+    model = Sequential()
+    model.add(LSTM(input_shape[0],activation="tanh", input_shape=input_shape))
+    model.add(Dropout(0.15))
+    model.add(Dense(1))
+    model.summary()
+
+    model.compile(optimizer="adam",loss="MSE")
+
+    return model
+
+def double_layer_lstm(input_shape: tuple):
+
+    model = Sequential()
+    model.add(LSTM(input_shape[0],activation="tanh", input_shape=(input_shape, return_sequences=True)))
+    model.add(Dropout(0.15))
+    model.add(LSTM(input_shape[0],activation="tanh", input_shape=input_shape))
+    model.add(Dense(1))
+    model.summary()
+
+    model.compile(optimizer="adam",loss="MSE")
+
+    return model
+    
+def bidirectional_lstm(input_shape: tuple):
+
+    model = Sequential()
+    model.add(Bidirectional(LSTM(input_shape[0],activation="tanh"), input_shape=input_shape))
+    model.add(Dropout(0.15))
+    model.add(Dense(1))
+    model.summary()
+
+    model.compile(optimizer="adam",loss="MSE")
+
+    return model
+
+def cnn1d(input_shape: tuple):
+
+    model = Sequential()
+    model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=input_shape))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    
+    return model
